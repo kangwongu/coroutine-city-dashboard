412cf6e09beb89e7b8b0401abd6a1454661287b8
kangwongu <kwg0527@naver.com>
2026-08-29 20:30:48 +0900

    feat: springdoc-openapi로 Swagger UI 추가
    curl 없이도 브라우저에서 API를 직접 호출해 확인할 수 있도록
    springdoc-openapi 의존성을 추가하고 API 문서/Swagger UI 경로를
    application.yml에 명시했다.

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.5")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
